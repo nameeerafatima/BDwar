3b85aeedb26ddda052dfc49e1a9e39474b9af852
nameeerafatima <123628472+nameeerafatima@users.noreply.github.com>
2023-01-26 16:34:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import {Routes, Route} from 'react-router-dom'
-import {Index} from './Components/Index'
-import {Dashboard} from './Components/Dashboard'
-import {AllJobs} from './Components/AllJobs'
-import { AboutUs } from './Components/AboutUs'
-import {Contact} from './Components/Contact'
-import {LogIn} from './Components/LogIn'
-import {SignUp} from './Components/SignUp'
-import { Edit } from './Components/Edit'
-// import { NewNavbar } from './Components/NewNavbar'
-import { Navbar } from './Components/Navbar'
-import { SampleFetch } from './Components/SampleFetch'
-import { NewDash } from './Components/NewDash'
+import Index from './Components/Index'
+import AllJobs from './Components/AllJobs'
+import AboutUs from './Components/AboutUs'
+import Contact from './Components/Contact'
+import LogIn from './Components/LogIn'
+import SignUp from './Components/SignUp'
+import Edit from './Components/Edit'
+import Navbar from './Components/Navbar'
+import NewDash from './Components/NewDash'
 
 
 function App() {
   return (
     <>
-    {/* <NewNavbar/> */}
     <Navbar/>
-    
     <Routes>
       <Route path='/' element={<Index/>}></Route>
       <Route path='/dashboard' element={<NewDash/>}></Route>
@@ -30,7 +25,6 @@ function App() {
       <Route path='/signup' element={<SignUp/>}></Route>
       <Route path='/edit' element={<Edit/>}></Route>
     </Routes>
-    {/* <SampleFetch/> */}
     </>
   );
 }
